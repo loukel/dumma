0e95604c214415fc9b7cfb3b5e6d4bfced9f8c63
loukel <louis.kelly1@hotmail.com>
2022-10-04 22:55:34 +0100

    Added line spacing to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
           <img src={logo} alt='logo' style={{ maxWidth: '60px' }}/>
           <span className="ml-3 text-2xl font-black text-logo-yellow">Durham University MMA</span>
         </Link>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center leading-10">
           <Link to='/' className="mr-5 hover:text-white">About</Link>
           <Link to='/team' className="mr-5 hover:text-white">Meet the Team</Link>
           <Link to='/contact-us' className="mr-5 hover:text-white">Contact Us</Link>
