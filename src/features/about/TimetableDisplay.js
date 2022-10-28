23cafc656b14f7e73e8a09ef8a54ea8bba603a3b
loukel <louis.kelly1@hotmail.com>
2022-10-28 23:22:17 +0100

    Updated timetable

--- src/features/about/TimetableDisplay.js
+++ src/features/about/TimetableDisplay.js
@@ -3,8 +3,8 @@ const TimetableDisplay = () => {
     <section className='text-center'>
       <h1 className="text-3xl font-bold text-center mb-5">Timetable</h1>
       <p className="p-2">Mondays 4-6pm at Maiden Castle Dojo – BJJ w/5th Element</p>
-      <p className="p-2">Wednesday 5-7pm at Maiden Castle Dojo – MMA w/Stephen</p> 
-      <p className="p-2">Saturday 10-12pm at Maiden Castle Dojo – MMA w/Stephen</p> 
+      <p className="p-2">Wednesday 5-7pm at Maiden Castle Dojo – MMA w/Itaipava</p> 
+      <p className="p-2">Saturday 10-12pm at Maiden Castle Dojo – MMA w/Itaipava</p> 
       <p className="p-2">Sunday 10-12pm at 5th Element Dojo – BJJ w/5th Element</p> 
       <p className="p-2">Sunday 3-5pm at Maiden Castle – Kickboxing w/Ironworks</p> 
       {/* Use grid instead wtih squares from monday to sunday */}
@@ -12,4 +12,4 @@ const TimetableDisplay = () => {
   )
 }
  
-export default TimetableDisplay;
+export default TimetableDisplay
