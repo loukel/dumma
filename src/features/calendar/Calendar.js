ec07d776be9a0284feee7f00282758ad4f747f9d
loukel <louis.kelly1@hotmail.com>
2023-01-30 14:41:23 +0000

    Updated calender

--- src/features/calendar/Calendar.js
+++ src/features/calendar/Calendar.js
@@ -8,36 +8,11 @@ const Calendar = () => {
         <div className='p-4 flex justify-center text-lg'><div className='bg-red-600 mr-2' style={{ width: '20px', height: '20px'}}></div>External Competition</div>
       </div>
       <div className='p-6'>
-        <h1 className='text-3xl font-bold text-center mb-6'>November</h1>
-        <section className='text-gray-800'>
-          <div className='grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-6'>
-            <CalendarCard colour={'bg-green-500'} title={'K1: In-House Comp'} location={'Durham - MC Dojo'} date={'26th November'}/>
-            <CalendarCard colour={'bg-green-500'} title={'BJJ: In-House Comp'} location={'Durham - 5th Element'} date={'27th November'}/>
-          </div>
-        </section>
-      </div>
-      <div className='p-6'>
-        <h1 className='text-3xl font-bold text-center mb-6'>December</h1>
-        <section className='text-gray-800'>
-          <div className='grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-6'>
-            <CalendarCard colour={'bg-red-600'} title={'BJJ: Open No-Gi'} location='Coventry' date={'3rd December'}/>
-            <CalendarCard colour={'bg-red-600'} title={'MMA: GUFS-06'} location='Watford' date={'4th December'}/>
-          </div>
-        </section>
-      </div>
-      <div className='p-6'>
-        <h1 className='text-3xl font-bold text-center mb-6'>January</h1>
-        <section className='text-gray-800'>
-          <div className='grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-6'>
-            <CalendarCard colour={'bg-red-600'} title={'BJJ: Grappling Industries'} location='Edinburgh' date={'14th January'}/>
-          </div>
-        </section>
-      </div>
-      <div className='p-6'>
         <h1 className='text-3xl font-bold text-center mb-6'>February</h1>
         <section className='text-gray-800'>
           <div className='grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-6'>
             <CalendarCard colour={'bg-red-600'} title={'BJJ: Allstars'} location='Manchester' date={'4th February'}/>
+            <CalendarCard colour={'bg-green-500'} title={'BJJ: In-House Comp'} location="5th Element's Gym" date={'5th February'}/>
             <CalendarCard colour={'bg-red-600'} title='MMA: Inter-Uni MMA Comp' location='Bristol' date={'25th February'}/>
           </div>
         </section>
@@ -46,7 +21,16 @@ const Calendar = () => {
         <h1 className='text-3xl font-bold text-center mb-6'>March</h1>
         <section className='text-gray-800'>
           <div className='grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-6'>
+          <CalendarCard colour={'bg-green-500'} title={'MMA: In-House Comp'} location='Maiden Castle' date={'4th March'}/>
             <CalendarCard colour={'bg-red-600'} title={'MMA: Inter-Uni MMA Event'} location='Birmingham' date={'26th March'}/>
+          </div>
+        </section>
+      </div>
+      <div className='p-6'>
+        <h1 className='text-3xl font-bold text-center mb-6'>April</h1>
+        <section className='text-gray-800'>
+          <div className='grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-6'>
+            <CalendarCard colour={'bg-red-600'} title={'BJJ: Scottish Open (ADCC)'} location='Scotland' date={'2nd April'}/>
           </div>
         </section>
       </div>
